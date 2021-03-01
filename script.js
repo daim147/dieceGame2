c6d9cd970f2105fbed282814746c4db3d9050061
Husnain Syed <69011172+DaimSyed@users.noreply.github.com>
2021-02-28 21:13:15 -1200

    Update script.js

--- script.js
+++ script.js
@@ -1,151 +1,202 @@
-const newbtn = document.querySelector(".btn--new")
-const holdbtn = document.querySelector(".btn--hold")
-const rollbtn = document.querySelector(".btn--roll")
-
-const currentScoreONE = document.querySelector("#current--0")
-const currentScoreTWO = document.querySelector("#current--1")
-
-const totalScoreONE = document.querySelector("#score--0")
-const totalScoreTWO = document.querySelector("#score--1")
-
-let bothPlayer = document.querySelectorAll(".player")
-const playerONE = document.querySelector('.player--0')
-const playerTWO = document.querySelector('.player--1')
-const imgDice = document.querySelector(".dice")
-let active = true
-let score ;
-starting()
-function starting(){
-    currentScoreONE.innerText = 0
-    currentScoreTWO.innerText = 0
-    totalScoreONE.innerText = 0
-    totalScoreTWO.innerText = 0
-    score = 0
-    playerONE.classList.add("player--active")
-    imgDice.classList.add('hidden')
+const newbtn = document.querySelector(".btn--new");
+const holdbtn = document.querySelector(".btn--hold");
+const rollbtn = document.querySelector(".btn--roll");
+const bothCurrent = document.querySelectorAll(".current-score");
+const bothScore = document.querySelectorAll(".score");
+const imgDice = document.querySelector(".dice");
+let bothPlayer = document.querySelectorAll(".player");
+let score;
+let idx = false;
+let one = 0;
+let three = 0;
+starting();
+function starting() {
+  idx = false;
+  bothPlayer[`${+idx}`].classList.add("player--active");
+  imgDice.classList.add("hidden");
+  score = 0;
+  one = 0;
+  three = 0;
 }
-newbtn.addEventListener("click",()=>{
-    window.location.reload()
-})
-rollbtn.addEventListener("click", ()=>{
-    play("yellow")
-    makingRandom()
-    
+newbtn.addEventListener("click", () => {
+  window.location.reload();
+});
+rollbtn.addEventListener("click", () => {
+  play("yellow");
+  makingRandom();
+});
 
- 
-})
-let one = 0
-let three = 0
-idx = 0
-holdbtn.addEventListener("click",()=>{
-    play("blue")
-    score = 0
-    active = false
-    if(idx == 0){
-       
-        let two;
-        
-        
-        two = +document.querySelector(`#current--${idx}`).textContent
-        one = one + two
-        
-        document.querySelector(`#score--${idx}`).textContent = one
-        
-        document.querySelector(`#current--${idx}`).textContent = 0
-        checkin(idx)
-        document.querySelector(`.player--${idx}`).classList.remove
-        ("player--active")
-        idx ++
-        document.querySelector(`.player--${idx}`).classList.add("player--active")
-    }else{
-        let four; 
-        
-       
-        four = +document.querySelector(`#current--${idx}`).textContent
-        three = three + four
-        
-        document.querySelector(`#score--${idx}`).textContent = three
-        document.querySelector(`#current--${idx}`).textContent = 0
+function removingAndActive(idx) {
+  bothPlayer[`${+idx}`].classList.remove("player--active");
+  idx = +!idx;
+  bothPlayer[`${+idx}`].classList.add("player--active");
+}
+function addingHighScore(num) {
+  bothScore[`${+idx}`].textContent = num;
+  bothCurrent[`${+idx}`].textContent = 0;
 
-        document.querySelector(`.player--${idx}`).classList.remove("player--active")
-        checkin(idx)
-        idx --
-        document.querySelector(`.player--${idx}`).classList.add("player--active")
+  if (num >= 20) {
+    play("victory");
+    bothPlayer[`${+idx}`].classList.remove("player--active");
+    bothPlayer[`${+idx}`].classList.add("player--winner");
+    setTimeout(() => {
+      window.location.reload();
+    }, 5000);
+  }
+}
+
+holdbtn.addEventListener("click", () => {
+  play("blue");
+  score = 0;
+  if (!idx) {
+    one = one + Number(bothCurrent[`${+idx}`].textContent);
+    addingHighScore(one);
+    removingAndActive(idx);
+    idx = +!idx;
+  } else {
+    three = three + Number(bothCurrent[`${+idx}`].textContent);
+    addingHighScore(three);
+    removingAndActive(idx);
+    idx = +!idx;
+  }
+});
+
+function makingRandom() {
+  let random = Math.ceil(Math.random() * 6);
+  imgDice.classList.remove("hidden");
+  imgDice.src = `dice-${random}.png`;
+  score += random;
+
+  if (random !== 1) {
+    bothCurrent[`${+idx}`].textContent = score;
+  } else {
+    score = 0;
+    bothCurrent[`${+idx}`].textContent = 0;
+    bothScore[`${+idx}`].textContent = 0;
+
+    if (!idx) {
+      one = 0;
+      removingAndActive(idx);
+      idx = +!idx;
+    } else {
+      three = 0;
+      removingAndActive(idx);
+      idx = +!idx;
     }
-})
-
-
-function makingRandom(){
-    let random = Math.ceil(Math.random() * 6)
-    imgDice.classList.remove('hidden')
-    imgDice.src = `dice-${random}.png`
-    score += random
-   
-
-    if(random !== 1){
-        document.querySelector(`#current--${idx}`).textContent = score
-        
-    }else{
-        score = 0   
-        document.querySelector(`#current--${idx}`).textContent = 0
-        document.querySelector(`#score--${idx}`).textContent  = 0
-        if(idx == 0){
-            one = 0
-            document.querySelector(`.player--${idx}`).classList.remove("player--active")
-            idx ++
-            document.querySelector(`.player--${idx}`).classList.add("player--active")
-        }else{
-            three = 0
-            document.querySelector(`.player--${idx}`).classList.remove("player--active")
-            idx --
-            document.querySelector(`.player--${idx}`).classList.add("player--active")
-        }
- 
-    }
+  }
 }
 function play(sound) {
-    var audio = new Audio(`${sound}.mp3`);
-    audio.play();
-  }
+  var audio = new Audio(`${sound}.mp3`);
+  audio.play();
+}
+// const currentScoreONE = document.querySelector("#current--0");
+// const currentScoreTWO = document.querySelector("#current--1");
+// const totalScoreONE = document.querySelector("#score--0");
+// const totalScoreTWO = document.querySelector("#score--1");
+// const playerONE = document.querySelector(".player--0");
+// const playerTWO = document.querySelector(".player--1");
+//   currentScoreONE.innerText = 0;
+//   currentScoreTWO.innerText = 0;
+//   totalScoreONE.innerText = 0;
+//   totalScoreTWO.innerText = 0;
 
-  function checkin(idx){
-    console.log("hyyy")
-    if(document.querySelector(`#score--${idx}`).textContent >= 30){
-        
-        document.querySelector(`.player--${idx}`).classList.remove("player--active")
-        play("victory")
-        rollbtn.removeEventListener("click", ()=>{
-            makingRandom()
-        })
-        one = 0
-        three = 0
-        starting()
-        document.querySelector(`.player--${idx}`).classList.add("player--winner")
+//   function winner(idx){
 
-    }
-  }
+//     if(document.querySelector(`#score--${idx}`).textContent >= 30){
 
+//         document.querySelector(`.player--${idx}`).classList.remove("player--active")
+//         play("victory")
+//         starting()
+//         document.querySelector(`.player--${idx}`).classList.add("player--winner")
 
+//     }
+//   }
 
+// let one = 0
+// let three = 0
+// idx = 0
+// holdbtn.addEventListener("click",()=>{
+//     play("blue")
+//     score = 0
+//     active = false
+//     if(idx == 0){
 
+//         let two;
 
+//         two = +document.querySelector(`#current--${idx}`).textContent
+//         one = one + two
 
+//         document.querySelector(`#score--${idx}`).textContent = one
 
+//         document.querySelector(`#current--${idx}`).textContent = 0
+//         checkin(idx)
+//         document.querySelector(`.player--${idx}`).classList.remove
+//         ("player--active")
+//         idx ++
+//         document.querySelector(`.player--${idx}`).classList.add("player--active")
+//     }else{
+//         let four;
 
+//         four = +document.querySelector(`#current--${idx}`).textContent
+//         three = three + four
 
+//         document.querySelector(`#score--${idx}`).textContent = three
+//         document.querySelector(`#current--${idx}`).textContent = 0
 
+//         document.querySelector(`.player--${idx}`).classList.remove("player--active")
+//         idx --
+//         document.querySelector(`.player--${idx}`).classList.add("player--active")
+//     }
+// })
 
+// function makingRandom(){
+//     let random = Math.ceil(Math.random() * 6)
+//     imgDice.classList.remove('hidden')
+//     imgDice.src = `dice-${random}.png`
+//     score += random
 
+//     if(random !== 1){
+//         document.querySelector(`#current--${idx}`).textContent = score
 
+//     }else{
+//         score = 0
+//         document.querySelector(`#current--${idx}`).textContent = 0
+//         document.querySelector(`#score--${idx}`).textContent  = 0
+//         if(idx == 0){
+//             one = 0
+//             document.querySelector(`.player--${idx}`).classList.remove("player--active")
+//             idx ++
+//             document.querySelector(`.player--${idx}`).classList.add("player--active")
+//         }else{
+//             three = 0
+//             document.querySelector(`.player--${idx}`).classList.remove("player--active")
+//             idx --
+//             document.querySelector(`.player--${idx}`).classList.add("player--active")
+//         }
 
+//     }
+// }
+// function play(sound) {
+//     var audio = new Audio(`${sound}.mp3`);
+//     audio.play();
+//   }
 
+//   function checkin(idx){
+//     console.log("hyyy")
+//     if(document.querySelector(`#score--${idx}`).textContent >= 30){
 
+//         document.querySelector(`.player--${idx}`).classList.remove("player--active")
+//         play("victory")
+//         rollbtn.removeEventListener("click", ()=>{
+//             makingRandom()
+//         })
+//         one = 0
+//         three = 0
+//         starting()
+//         document.querySelector(`.player--${idx}`).classList.add("player--winner")
 
-
-
-
-
-
+//     }
+//   }
 
 // let idx = 0
 
@@ -165,14 +216,13 @@ function play(sound) {
 //      idx++
 //      bothPlayer[idx].classList.add("player--active")
 //     }
-      
-      
+
 //     })
 
 // function playerId(random){
-    
+
 //     bothPlayer[idx].classList.add("player--active")
-    
+
 //        if(random !== 1){
 //            document.querySelector(`#current--${idx}`).textContent = score
 //         //    if(!active){
@@ -194,10 +244,9 @@ function play(sound) {
 //             }else{
 //              idx++
 //             }
-               
-               
+
 //            }
-               
+
 // }
 //    }else{
 //        console.log(idx, "one activating" )
@@ -207,12 +256,8 @@ function play(sound) {
 //            console.log(idx, "two activating" )
 //            active = true
 //        }
-       
+
 //    }
-
-
-
-
 
 // function playerId(random){
 //      bothPlayer.forEach((player,idx)=>{
@@ -231,8 +276,7 @@ function play(sound) {
 //                 console.log("hello")
 //                 active = false
 //             }
-                
-            
+
 //         }
 //     }else{
 //         console.log(idx, "one activating" )
@@ -242,13 +286,12 @@ function play(sound) {
 //             console.log(idx, "two activating" )
 //             active = true
 //         }
-        
+
 //     }
 
 //  })
 
 // }
-
 
 // function addingNumbersTo(idx,random){
 //     if(random === 1){
@@ -260,5 +303,3 @@ function play(sound) {
 //         document.querySelector(`#current--${idx}`).textContent = score
 //     }
 // }
-
-
